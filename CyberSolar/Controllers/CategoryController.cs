268a0721af644ddc69b854ec1c0e5c1779460805
Shiiifuuuu <mdshahadat.hossain7@gmail.com>
2019-11-24 11:00:03 +0600

    4FormDone

--- CyberSolar/Controllers/CategoryController.cs
+++ CyberSolar/Controllers/CategoryController.cs
@@ -41,8 +41,6 @@ namespace CyberSolar.Controllers
                 if (_categoryManager.Add(category))
                 {
                     message += "Saved!";
-                    ModelState.Clear();
-                    return View();
                 }
                 else
                 {
@@ -51,6 +49,119 @@ namespace CyberSolar.Controllers
             }
             categoryViewModel.Categories = _categoryManager.GetAll();
             ViewBag.Message = message;
+            return View(categoryViewModel);
+        }
+        [HttpGet]
+        public ActionResult Search()
+        {
+            CategoryViewModel categoryViewModel = new CategoryViewModel();
+            categoryViewModel.Categories = _categoryManager.GetAll();
+
+
+            return View(categoryViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Search(CategoryViewModel categoryViewModel)
+        {
+            var categories = _categoryManager.GetAll();
+
+            if (categoryViewModel.Code != null)
+            {
+                categories = categories.Where(c => c.Code.Contains(categoryViewModel.Code)).ToList();
+            }
+
+            if (categoryViewModel.Name != null)
+            {
+                categories = categories.Where(c => c.Name.ToLower().Contains(categoryViewModel.Name.ToLower())).ToList();
+            }
+
+            categoryViewModel.Categories = categories;
+
+
+            return View(categoryViewModel);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var category = _categoryManager.GetById(id);
+
+            CategoryViewModel categoryViewModel = Mapper.Map<CategoryViewModel>(category);
+
+            categoryViewModel.Categories = _categoryManager.GetAll();
+
+            return View(categoryViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(CategoryViewModel categoryViewModel)
+        {
+            string message = "";
+
+            if (ModelState.IsValid)
+            {
+                Category category = Mapper.Map<Category>(categoryViewModel);
+
+                if (_categoryManager.Update(category))
+                {
+                    message = "Updated";
+                }
+                else
+                {
+                    message = "Not Updated";
+                }
+            }
+            else
+            {
+                message = "Model State False!!";
+            }
+
+            ViewBag.Message = message;
+            categoryViewModel.Categories = _categoryManager.GetAll();
+
+            return View(categoryViewModel);
+        }
+
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var category = _categoryManager.GetById(id);
+
+            CategoryViewModel categoryViewModel = Mapper.Map<CategoryViewModel>(category);
+
+            categoryViewModel.Categories = _categoryManager.GetAll();
+
+            return View(categoryViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(CategoryViewModel categoryViewModel)
+        {
+            string message = "";
+
+            if (ModelState.IsValid)
+            {
+                Category category = Mapper.Map<Category>(categoryViewModel);
+
+                if (_categoryManager.Delete(category.Id))
+                {
+                    message = "Deleted";
+                }
+                else
+                {
+                    message = "Not Deleted";
+                }
+            }
+            else
+            {
+                message = "Model State False!!";
+            }
+
+            ViewBag.Message = message;
+            categoryViewModel.Categories = _categoryManager.GetAll();
+
             return View(categoryViewModel);
         }
     }
